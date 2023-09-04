64cb45b3836ca63434c1f4b50eb3a5f3026b86bd
Datureli <soleris1@gmail.com>
2023-09-04 20:02:39 +0200

    add styles to navigation

--- resources/js/app.vue
+++ resources/js/app.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <Navigation /> <!-- Komponent nawigacji -->
-    <router-view></router-view> <!-- Zawartość trasy wyświetlana tutaj -->
-  </div>
+    <Navigation />
+    <router-view></router-view>
+  </div> 
 </template>
 
 <script setup>
